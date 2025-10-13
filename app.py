1eff1ef3fe20beb6760dd912700e0e6ff03acf2e
Aleksandr Kutuzov <alleteam@gmail.com>
2025-10-13 13:26:38 +0900

    App: video is now compatible with all supported browsers, wind direction in human readable form, mobile friendly layout, code cleanup

--- app.py
+++ app.py
@@ -3,11 +3,38 @@
 import os
 import asyncio
 
+from fastapi.responses import FileResponse
+
 from nicegui import app, ui, background_tasks
 from ecowitt_wn90lp.ws90 import WS90Client
 from datetime import datetime
 from pydantic_settings import BaseSettings, SettingsConfigDict
 from pydantic import Field
+
+TEXT_HEADER_SIZE = "text-h4"
+
+
+def degToCompass(num):
+    val = int((num / 22.5) + 0.5)
+    arr = [
+        "N",
+        "NNE",
+        "NE",
+        "ENE",
+        "E",
+        "ESE",
+        "SE",
+        "SSE",
+        "S",
+        "SSW",
+        "SW",
+        "WSW",
+        "W",
+        "WNW",
+        "NW",
+        "NNW",
+    ]
+    return arr[(val % 16)]
 
 
 class Settings(BaseSettings):
@@ -26,23 +53,22 @@ data = None
 @ui.refreshable
 def number_ui() -> None:
     if not data:
-        ui.label("Waiting for data").classes("text-h1")
+        ui.label("Waiting for data").classes(TEXT_HEADER_SIZE)
         return
 
-    ui.label("Wind").classes("text-h3")
+    ui.label("Wind").classes(TEXT_HEADER_SIZE)
     with ui.circular_progress(10, min=0, max=360, show_value=False) as progress:
         progress.classes("size-full").props(
             f"angle={data.wind_direction-5} color='red'"
         )
-        wind_text = "?"
-        if data.wind_speed > 0:
-            wind_text = f"{data.wind_direction}˚"
-        ui.label(wind_text).classes("text-h2")
+        wind_text = degToCompass(data.wind_direction)
+        ui.label(wind_text).classes("text-h3")
+        ui.label(f"{data.wind_direction}˚").classes("text-h5")
     ui.markdown(
         f"Speed: {data.wind_speed}m/s<br>" f"Gust: {data.gust_speed}m/s<br>"
     ).classes("text-h4")
 
-    ui.label("Other").classes("text-h3")
+    ui.label("Other").classes(TEXT_HEADER_SIZE)
     ui.markdown(
         f"Light: {data.light} lux<br>"
         f"UV Index: {data.uv_index}<br>"
@@ -57,23 +83,39 @@ def number_ui() -> None:
 
 @ui.page("/")
 def main_page() -> None:
-    with ui.element("div").classes("container mx-auto"):
-        with ui.grid(columns=16).classes("w-full"):
-            with ui.element().classes("col-span-12"):
+    ui.add_head_html(
+        '<link href="/static/video-js-8.21.1/video-js.min.css" rel="stylesheet">'
+    )
+    ui.add_head_html('<script src="/static/video-js-8.21.1/video.min.js"></script>')
+    ui.add_head_html(
+        "<style>.vjs-volume-panel, .q-loading-bar { display: none !important; }</style>"
+    )
+
+    with ui.element().classes("container mx-auto"):
+        with ui.element().classes("row q-col-gutter-md"):
+            with ui.element().classes("col-xs-12 col-lg-9"):
                 with ui.card():
-                    ui.label("Currently in the sky").classes("text-h3")
-                    ui.video(
-                        "/video/stream.m3u8",
-                        controls=True,
-                        autoplay=True,
-                        loop=True,
-                    ).classes("w-full aspect-video")
-            with ui.element().classes("col-span-4"):
+                    ui.label("Currently in the sky").classes(TEXT_HEADER_SIZE)
+                    with ui.element("video") as video:
+                        video.classes("video-js vjs-default-skin w-full vjs-fluid")
+                        video.props('autoplay controls preload="auto" data-setup="{}"')
+                        ui.element("source").props('src="/video/stream.m3u8"')
+            with ui.element().classes("col-xs-12 col-lg-3"):
                 with ui.card().classes("w-full"):
                     number_ui()
 
 
+@app.get("/video/stream.m3u8")
+def generate_random_number():
+    return FileResponse(
+        "video/stream.m3u8",
+        media_type="application/x-mpegurl",
+        headers={"Cache-Control": "no-cache"},
+    )
+
+
 app.add_static_files("/video", "video")
+app.add_static_files("/static", "static")
 
 
 async def backgroundRefreshData() -> None:
